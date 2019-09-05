718b1b8456713a3fc9035e6cab54107447348e08
Diego Azevedo <dapptn@hotmail.com>
2019-09-05 20:03:13 +1000

    Changed file store return

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -3,19 +3,19 @@ import File from '../models/File';
 class FileController {
   async store(req, res) {
     const { file = null } = req;
-    console.log(req.file);
+
     if (!file) return res.status(401).json({ error: 'File was not sent.' });
 
     file.name = req.file.originalname;
     file.path = req.file.filename;
 
     const fileSaved = await File.create(file);
-    return res.json(fileSaved);
-    // return res.json({
-    //   id: fileSaved.id,
-    //   name: fileSaved.name,
-    //   path: fileSaved.path,
-    // });
+
+    return res.json({
+      id: fileSaved.id,
+      url: fileSaved.url,
+      name: fileSaved.name,
+    });
   }
 }
 
---
+++ src/database/migrations/20190905063816-meetups.js
@@ -0,0 +1,64 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('meetups', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      decription: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      country: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      event_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      image_id: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'files',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      creator_id: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('meetups');
+  },
+};
