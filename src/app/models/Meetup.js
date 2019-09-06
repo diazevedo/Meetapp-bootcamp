e01ad85abd048810105568b614c8dc6aae45141c
Diego Azevedo <dapptn@hotmail.com>
2019-09-06 16:17:35 +1000

    Past virtual field add to MeetupModel

--- src/app/models/Meetup.js
+++ src/app/models/Meetup.js
@@ -1,4 +1,5 @@
 import { Sequelize, Model } from 'sequelize';
+import { isBefore } from 'date-fns';
 
 class Meetup extends Model {
   static init(sequelize) {
@@ -10,6 +11,12 @@ class Meetup extends Model {
         location: Sequelize.STRING,
         creator_id: Sequelize.INTEGER,
         file_id: Sequelize.INTEGER,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
       },
       {
         sequelize,
