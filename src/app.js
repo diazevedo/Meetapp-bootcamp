40b46fe9c7fe121449c4c4634d133feb5944d185
Diego Azevedo <dapptn@hotmail.com>
2019-09-07 21:16:47 +1000

    Sentry added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+
+import sentryConfig from './config/sentry';
 import './database';
 
 class App {
@@ -10,15 +15,23 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   /**
    * API can accept json
    */
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+
+    /**
+     * Set express to allow files be accessed using browser
+     */
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
@@ -31,6 +44,15 @@ class App {
    */
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://bbfba5778a8e4ce481388b50d8f52a4b@sentry.io/1563325',
+};
